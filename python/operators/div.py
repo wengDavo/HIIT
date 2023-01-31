a05fc0553400326967d10d2b146ec419a800bf05
wengDavo <immanueldavo@gmail.com>
2023-01-31 16:40:32 +0100

    operators

---
+++ python/operators/div.py
@@ -0,0 +1,9 @@
+print("*"*20)
+""" assigning users input to variable"""
+num_1 = int(input("Enter first value:"))
+num_2 = int(input("Enter second value:"))
+""" storing the division of the two variables in result"""
+result = num_1 / num_2
+""" printing the result using a formtted string"""
+print(f"The division of {num_1} and {num_2} : {result}")
+print("*"*20)
---
+++ python/operators/sub.py
@@ -0,0 +1,9 @@
+print("*"*20)
+""" assigning users input to variable"""
+num_1 = int(input("Enter first value:"))
+num_2 = int(input("Enter second value:"))
+""" storing the difference of the two variables in result"""
+result = num_1 - num_2
+""" printing the result using a formtted string"""
+print(f"The subtraction of {num_1} from {num_2} : {result}")
+print("*"*20)
